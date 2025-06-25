ebd9795793627f078c158b8588b3d1f7dba8f662
nithyaponugoti <nithya.ponugoti@gmail.com>
2025-06-25 12:42:30 -0700

    Comparison Operators

--- ComparisonOperators.c
+++ ComparisonOperators.c
@@ -3,11 +3,11 @@ int main()
 {
     int x=10; int y=20;
     printf("%d %d %d %d %d %d",
-        (x>y)
-        (x<y)
-        (x==y)
-        (x>=y)
-        (x<=y)
+        (x>y),
+        (x<y),
+        (x==y),
+        (x>=y),
+        (x<=y),
         (x!=y)
     );
     return 0;
