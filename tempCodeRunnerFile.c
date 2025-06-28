c3c5bc3f444d47aa42d3637c0828c7504e697ef7
nithyaponugoti <nithya.ponugoti@gmail.com>
2025-06-28 15:43:54 -0700

    Code Using If Else Commands

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-int main()
+int main() 
 {
-    int a= 15;
-    int n= 13;
-    int ans = (a * (n-1)*(n+1));
-    ans += 10;
-    ans %= 7;
-        printf("%d\n", ans);
+   int number=200; 
+    if(number>100)
+    printf("Big\n");
+    printf("Number\n");
 }
