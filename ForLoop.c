d553f65a886e8eb3657fbde3772a598b6a41a9e4
nithyaponugoti <nithya.ponugoti@gmail.com>
2025-07-10 11:50:34 -0700

    While Loop Code

--- ForLoop.c
+++ ForLoop.c
@@ -2,6 +2,6 @@
 int main()
 {
     for(int x=0; x<5; x++)
-    printf("HelloWorld");
+    printf("HelloWorld\n");
     return 0;
 }
---
+++ WhileLoop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i=5;
+    while (i<10)
+    {
+        printf("Hi!\n");
+        i++;
+    }
+    return 0;
+
+}
