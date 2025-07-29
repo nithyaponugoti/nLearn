fab5b9db28375e58abeb74970a61ce69ea513d6e
Nithya V Ponugoti <nithyavponugoti@Nageswaras-MBP.attlocal.net>
2025-07-29 15:08:54 -0700

    Pattern Searching in C

---
+++ patternsearching.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+void search(char* a, char* b)
+{
+	int M=strlen(a);
+	int N=strlen(b);
+	for(int i=0; i<=M-N;i++){
+		int j;
+		int x=i;
+		for(j=0; j<N; j++){
+			if(a[i+j]!= b[j])
+			break;
+		}
+		if(j==N)
+			printf("Pattern found at index %d\n", i);
+	}
+}
+int main()
+{
+	char a[]= "ABCDEFGH";
+	char b[]="ABCD";
+	search(a,b);
+	return 0;
+}
+	
+		
