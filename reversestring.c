f86d513c7e7472e6e0dafa16b4c091a9bbae75f9
Nithya V Ponugoti <nithyavponugoti@Nageswaras-MBP.attlocal.net>
2025-07-30 13:37:37 -0700

    Reversing a String

---
+++ reversestring.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+	char str1[]= "Hello";
+	int a = strlen(str1);
+	char newstring[10];
+	int j = 0;
+	for(int i= a - 1; i>=0; i--, j++){
+			 newstring[j] = str1[i];
+	}
+		newstring[a] = '\0';
+		printf("%s\n", newstring);
+		return 0;
+}
+	
