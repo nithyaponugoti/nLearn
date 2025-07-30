4b7cb866e0afb79e44237eafe4fdfb6fbb5349f8
Nithya V Ponugoti <nithyavponugoti@Nageswaras-MBP.attlocal.net>
2025-07-30 13:38:16 -0700

    Reversing a String

--- strncmp.c
+++ strncmp.c
@@ -2,11 +2,11 @@
 #include <string.h>
 int main()
 {
-	char str1[20]= "Hello World";
-	char str2[20]= "Hello";
-	if(strncmp(str1,str2,strlen(str2))==0)
-	printf("True\n");
+	char str1[20] = "Hello World";
+	char str2[20] = "Hello";
+	if (strncmp(str1, str2, strlen(str2)) == 0)
+		printf("True\n");
 	else
-	printf("False\n");
+		printf("False\n");
 	return 0;
 }
